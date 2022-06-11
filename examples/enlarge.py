2852c9329a6e5202b6fcd57d4073a96f9671d7a7
Deathrescheduler12 <104992858+Deathrescheduler12@users.noreply.github.com>
2022-06-11 06:29:05 +0300

    Create enlarge.py

---
+++ examples/enlarge.py
@@ -0,0 +1,8 @@
+point1 = Point((1, 1))
+point2 = Point((1, 3))
+point3 = Point((4, 2))
+for z in [point1, point2, point3]:
+    z.enlarge(np.array([1, 4]))
+plt.plot([point1.coordinates[0], point2.coordinates[0], point3.coordinates[0]], [point1.coordinates[1], point2.coordinates[1], point3.coordinates[1]], "ro")
+plt.axis([-2, 8, -2, 5])
+plt.show()

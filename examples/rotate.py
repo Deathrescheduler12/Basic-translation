462942b9e0146b46807474b3c725ba3867217731
Deathrescheduler12 <104992858+Deathrescheduler12@users.noreply.github.com>
2022-06-11 06:32:29 +0300

    Create rotate.py

---
+++ examples/rotate.py
@@ -0,0 +1,10 @@
+from ..point import Point
+angle = 90
+point1 = Point((1, 1))
+point2 = Point((1, 3))
+point3 = Point((4, 2))
+for z in [point1, point2, point3]:
+    z.rotate(angle)
+plt.plot([point1.coordinates[0], point2.coordinates[0], point3.coordinates[0]], [point1.coordinates[1], point2.coordinates[1], point3.coordinates[1]], "ro")
+plt.axis([-2, 8, -2, 5])
+plt.show()
